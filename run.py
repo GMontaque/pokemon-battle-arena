326e9622d99cf05a75f717ca34483526817d14c7
Giulio Montaque <giuliom@hotmail.co.uk>
2024-01-09 17:35:10 +0000

    create game start function

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def game_start():
+    '''
+    inital game function
+    called when game starts
+    Gets the users name and provides them with the rules of the game
+    '''
+    print('''welcome to pokemon battle area where trainers are tested''')
+    name = input("what is your name player: ")
+    print(f"Hello {name}, welcome my names professor oak are you I hope your ready")
+    print("*********************start insert rules of game********************")
+    print("content")
+    print("********************* end insert rules of game********************")
+    play = " "
+
+    # play_game()
+    while play.lower() not in ['yes', 'no']:
+        play = input("Are you ready to fight'yes' or 'no': ")
+        if play == "yes":
+            print("play Game")
+        else:
+            print("ok see you next time")
+
+game_start()
