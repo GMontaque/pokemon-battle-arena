0ebe3660527fd82c76d034557f02c67beac73f9f
Giulio Montaque <giuliom@hotmail.co.uk>
2024-01-09 17:51:19 +0000

    create play game function

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
+
+def play_game():
+    global pl_pokemon
+    print("game is running")
+    print("please select which pokemon you want to fight with: ")
+
+
+
 def game_start():
-    '''
-    inital game function
-    called when game starts
-    Gets the users name and provides them with the rules of the game
-    '''
     print('''welcome to pokemon battle area where trainers are tested''')
     name = input("what is your name player: ")
+    # name = "steve"
     print(f"Hello {name}, welcome my names professor oak are you I hope your ready")
     print("*********************start insert rules of game********************")
     print("content")
@@ -16,7 +20,7 @@ def game_start():
     while play.lower() not in ['yes', 'no']:
         play = input("Are you ready to fight'yes' or 'no': ")
         if play == "yes":
-            print("play Game")
+            play_game()
         else:
             print("ok see you next time")
 
