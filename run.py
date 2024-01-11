accbf7415d7131d5455a84ab869576ffb45e734c
Giulio Montaque <giuliom@hotmail.co.uk>
2024-01-11 19:11:40 +0000

    create second player name input and add in error catch

--- run.py
+++ run.py
@@ -23,28 +23,43 @@ def play_game(user1,user2):
     print(player2.name)
 
 def game_start():
-    print('''Proffessor Oak: welcome to pokemon battle area where trainers are tested''')
+    print("Proffessor Oak: welcome to pokemon battle area where trainers are tested")
     while True:
         try:
-            user_name = input("Proffessor Oak: what is your name player? ")
+            user_name = input("Proffessor Oak: what is your name young man? ")
             if not user_name:
                 raise ValueError("Proffessor Oak: Oops seems you forgot to enter your name, why not try again")
             elif not re.match("^[A-Za-z]+$", user_name):
                 raise ValueError("Proffessor Oak: Oops seems you entered a number, why not try again")
-            print(f"Proffessor Oak: Hello {user_name}, welcome my names professor oak, I hope your ready to fight")
-            print("*********************start insert rules of game********************")
-            print("content")
-            print("********************* end insert rules of game********************")
             break
         except ValueError as e:
             print(f"{e}")
+
+    print(f"Proffessor Oak: Well hello {user_name}, its nice to meet you, I see you also brought a friend")
+
+    while True:
+        try:
+            user_name2 = input("Hello friend and what might your name be? ")
+            if not user_name2:
+                raise ValueError("Proffessor Oak: Oops seems you forgot to enter your name, why not try again")
+            elif not re.match("^[A-Za-z]+$", user_name2):
+                raise ValueError("Proffessor Oak: Oops seems you entered a number, why not try again")
+           
+            break
+        except ValueError as e:
+            print(f"{e}")
+
+    print(f"Proffessor Oak: Well hello {user_name2} and {user_name}, welcome my names professor oak and I hope your ready to fight")
+    print("*********************start insert rules of game********************")
+    print("content")
+    print("********************* end insert rules of game********************")
 
     play = " "
 
     # play_game()
     while play.lower() not in ['fight', 'exit']:
         try:
-            play = input("Enter 'fight' when your ready to contiune or 'exit' to exit the game: ")
+            play = input("Proffessor Oak: Enter 'fight' when your ready to contiune or 'exit' to exit the game: ")
             if play.lower() not in ['fight', 'exit']:
                     raise ValueError("Proffessor Oak: Oops, close but I need an answer, either fight or exit")
             elif play == "fight":
