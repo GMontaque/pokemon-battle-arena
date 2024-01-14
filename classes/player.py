6b0d3cecbe10bcd8ad6f1ab06f78ee44a2009ad0
Giulio Montaque <giuliom@hotmail.co.uk>
2024-01-14 16:53:31 +0000

    add description comments to pick_pokemon method

--- classes/player.py
+++ classes/player.py
@@ -11,6 +11,28 @@ class Player:
         self.is_human = kwargs["is_human"]
         # stores the pokemon picked for each player
         self.battle_pokemon = {}
+        # self.battle_pokemon = {
+        #     "s": {"name": "squirtle",
+        #             "description": "turtle like pokemon that likes "
+        #                     "to make jokes and enjoys a good swin",
+        #                     "pokemon_type": "water",
+        #                     "health": 120,
+        #                     "attacks": {"s1": 20, "s2": 30, "s3": 40, "s4": 50}},
+
+        #     "c": {"name": "charmander",
+        #         "description": "fire like pokemon that likes "
+        #                     "can have a temper so be careful not to anger",
+        #                     "pokemon_type": "fire",
+        #                     "health": 120,
+        #                     "attacks": {"c1": 20, "c2": 30, "c3": 40, "c4": 50}},
+
+        #     "b": {"name": "bulbasaur",
+        #         "description": "green type pokemon that can be found "
+        #                     "in the surroudning forests enjoing the sun",
+        #                     "pokemon_type": "earth",
+        #                     "health": 120,
+        #                     "attacks": {"b1": 20, "b2": 30, "b3": 40, "b4": 50}}
+        # }
 
     def pick_pokemon(self):
         '''players are asked to pick 3 pokemon they wish to fight with '''
@@ -22,8 +44,7 @@ class Player:
                       "Charmander or Bulbasaur")
 
                 # user inputs pokemon name and value is checked
-                pokemon_name_input = True
-                while pokemon_name_input:
+                while True:
                     try:
                         # input pokemon name
                         pokemon_name = input("Which pokemon would you like "
@@ -46,29 +67,30 @@ class Player:
                                              "one of the pokemon you can "
                                              " choose, please try again")
                         else:
-                            # once user enters a correct value, resets loop
-                            pokemon_name_input = False
+                            # once user enters a correct value, exits loop
+                            break
                     except ValueError as e:
                         print(f"{e}")
 
                 ''' Display the pokemon details i.e description,
                 pokemon type and attacks
                 '''
-
+                #description
                 print(f"{pokemon_name} is a "
                       f"{pokedex[pokemon_name]['description']}")
 
+                # pokemon type
                 print(f"{pokemon_name} is a "
                       f"{pokedex[pokemon_name]['pokemon_type']} type pokemon")
 
+                # pokemon attack list and values
                 print(f"{pokemon_name} has the following attacks "
                       f"{pokedex[pokemon_name]['attacks']}")
 
                 ''' Asks the user if they want
-                to add the pokemon to there battle pack
+                to add the pokemon to their battle pack
                 '''
-                pokemon_pick_input = True
-                while pokemon_pick_input:
+                while True:
                     try:
                         # input players choice on pokemon
                         picking_pokemon = input(f"Do you want to add "
@@ -84,7 +106,7 @@ class Player:
                                              " a 'YES' or 'NO' answer")
                         else:
                             # once user enters a correct value, resets loop
-                            pokemon_pick_input = False
+                            break
                     except ValueError as e:
                         print(f"{e}")
 
