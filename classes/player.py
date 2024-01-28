ce87698ee0d38372851b0d159c67fa666b8e4a18
Giulio Montaque <giuliom@hotmail.co.uk>
2024-01-28 11:10:10 +0000

    update variable game_notification with a new colour white

--- classes/player.py
+++ classes/player.py
@@ -9,7 +9,7 @@ from colorama import Fore, Back, Style
 
 error_colour = Back.RED + Fore.WHITE
 reset_styling = Style.RESET_ALL = Style.RESET_ALL
-game_notification = Back.BLUE + Fore.GREEN
+game_notification = Back.BLUE + Fore.White
 proffessor_oak = (Style.RESET_ALL + Fore.GREEN + "Proffessor Oak: "
                   + Style.RESET_ALL)
 
